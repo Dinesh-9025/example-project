11d933ee0a3184d08a5c96d381c17fd24e2f23e3
Dinesh-9025 <104342061+Dinesh-9025@users.noreply.github.com>
2022-04-27 18:30:55 +0530

    Add files via upload

---
+++ 27.4.2022(road 2)/WP lifecycle with js.js
@@ -0,0 +1,21 @@
+console.log(window.screen.width, window.screen.height);
+
+console.log(window.innerWidth, window.innerHeight);
+
+console.log(window.location);
+
+console.log(window);
+
+window.moveTo(500, 100);
+
+window.open("https://www.youtube.com/");
+
+window.alert("Hello everyone");
+
+window.prompt("what is your name");
+
+window.print("Gonna take screenshot")
+
+//setInterval(() => {
+//    location.href = "https://www.Google.com/";   
+//}, 1000);
